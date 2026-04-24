42e69268cae44edac007f00ab514ddfd8f8ab8f1
GauriNandha <gaurinandha020@gmail.com>
2026-04-24 09:21:00 +0530

    [GauriNandha] Added [UC5]

--- App/src/TrainApp.java
+++ App/src/TrainApp.java
@@ -1,29 +1,17 @@
-import java.util.LinkedList;
+import java.util.LinkedHashSet;
 
 public class TrainApp {
 
     public static void main(String[] args) {
 
         System.out.println("=== Train Consist Management App ===");
-        LinkedList<String> trainConsist = new LinkedList<>();
-        trainConsist.add("Engine");
-        trainConsist.add("Sleeper");
-        trainConsist.add("AC");
-        trainConsist.add("Cargo");
-        trainConsist.add("Guard");
-
-        System.out.println("\nInitial Train Consist:");
-        System.out.println(trainConsist);
-        trainConsist.add(1, "Pantry Car");
-
-        System.out.println("\nAfter adding Pantry Car at position 2:");
-        System.out.println(trainConsist);
-        trainConsist.removeFirst();
-        trainConsist.removeLast();
-
-        System.out.println("\nAfter removing first and last bogie:");
-        System.out.println(trainConsist);
-        System.out.println("\nFinal Train Consist:");
-        System.out.println(trainConsist);
+        LinkedHashSet<String> trainFormation = new LinkedHashSet<>();
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
+        trainFormation.add("Sleeper");
+        System.out.println("\nFinal Train Formation (No duplicates, insertion order preserved):");
+        System.out.println(trainFormation);
     }
 }
